b5e45e50ab6a78158f2629fd4526e582e246eecd
Dmitriy Vasilev <serious2989@gmail.com>
2022-12-13 11:15:29 +0300

    6 task

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-# 5. Напишите программу, которая принимает на вход число и проверяет,
-#    кратно ли оно 5 и 10 или 15, но не 30.
+# 6. Пример проверки ложности утверждения (x ≡ z ) ∨ (x → (y ∧ z))
+# задача дз
 
-num = int(input("Input your number: "))
-if (num % 5 == 0 and num % 10 == 0 or num % 15 == 0) and num % 30 != 0:
-     print("Yes")
-else:
-     print("No")
+print("x y z")
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x == z or x <= y and z):
+                print(x,y,z)
+                
