f3eeed92e451ec1ba75f7054e28913f351ff1b72
Dmitriy <serious2989@gmail.com>
2022-12-20 17:42:25 +0300

    task1_dz

--- homeworks/lesson_2_dz/task_1.py
+++ homeworks/lesson_2_dz/task_1.py
@@ -2,3 +2,15 @@
 # вещественное число и показывает сумму его цифр.
 # Без работы с методами строк.
 
+number = input()
+sum_digits = 0
+
+power = len(number) - 2
+number = float(number)
+number *= int(10 ** power)
+
+while number:
+    sum_digits += int(number % 10)
+    number //= 10
+
+print (int(sum_digits))  
