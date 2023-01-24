564985acdad320e652429ed7eb209bcb0cd17e28
Dmitriy <serious2989@gmail.com>
2023-01-24 17:22:35 +0300

    task_2__dz

--- homeworks/lesson_6_dz/task_2_dz.py
+++ homeworks/lesson_6_dz/task_2_dz.py
@@ -1,5 +1,6 @@
 # 2. Для чисел в пределах от 20 до n найти числа,
 #    кратные 20 или 21. Use comprehension.
+# in 100 in 424
 
 def uniq_list(num):
     return [el for el in range(20, num + 1) if el % 20 == 0 or el % 21 == 0]
